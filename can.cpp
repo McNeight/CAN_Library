2d59a92d94d5d229bdc57f419d73163ebf2bd163
DRO <cevallos.pedro@gmail.com>
2014-05-07 19:51:59 -0400

    Update can.cpp

--- can.cpp
+++ can.cpp
@@ -1,8 +1,8 @@
 /* Copyright (C) 2014
 
-	Contributor:  Pedro Cevallos
+   Contributor:  Pedro Cevallos
 
-	This program is free software: you can redistribute it and/or modify
+    This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
@@ -15,12 +15,12 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-	Acknowledgments:
-	Fabian Greif for the initial MCP2515 library http://www.kreatives-chaos.com/artikel/universelle-can-bibliothek
-	David Harding for his version of the MCP2515 library http://forum.arduino.cc/index.php/topic,8730.0.html
-	Kyle Crockett CANduino library with 16Mhz oscillator. (http://code.google.com/p/canduino/)
-	Nuno Alves for the help on Extended ID messaging
-	Stevenh for his work on library and all of the MCP research/work http://modelrail.otenko.com/arduino/arduino-controller-area-network-can
+Acknowledgments:
+Fabian Greif for the initial MCP2515 library http://www.kreatives-chaos.com/artikel/universelle-can-bibliothek
+David Harding for his version of the MCP2515 library http://forum.arduino.cc/index.php/topic,8730.0.html
+Kyle Crockett CANduino library with 16Mhz oscillator. (http://code.google.com/p/canduino/)
+Nuno Alves for the help on Extended ID messaging
+Stevenh for his work on library and all of the MCP research/work http://modelrail.otenko.com/arduino/arduino-controller-area-network-can
 
 -------------------------------------------------------------------------------------------------------------
 Change Log
@@ -29,7 +29,8 @@ DATE		VER		WHO			WHAT
 07/07/13	0.1		PC		Modified and merge all MCP2515 libraries found. Stripped away most unused fuctions and corrected MCP2515 defs
 09/12/13	0.2		PC		Added selectable CS SPI for CAN controller to use 1 IC to control several mcp2515
 02/05/14	0.3		PC		Added filter and mask controls
-05/01/14	0.4		PC		Cleaned up fuctions, variables and added message structures for J1939, CANopen and CAN. 
+05/01/14	0.4		PC		Cleaned up fuctions, variables and added message structures for J1939, CANopen and CAN.
+05/07/14	1.0		PC		Released Library to the publich through gitHub
 -------------------------------------------------------------------------------------------------------------
 
 Features:
@@ -53,9 +54,9 @@ Desgined to be used with ATMEL ATMega328P with Arduino bootlader, MCP2515 Stand-
 #include "can.h"
 
 ///////////////////////////////////////////////////////////////////
-///																///	
-///							CAN 								///
-///																///
+///							        ///	
+///	           CAN library for MCP2515		 	///
+///								///
 ///////////////////////////////////////////////////////////////////
 
 
@@ -299,7 +300,7 @@ void MCP::clearRxBuffers()
 	SPI.transfer (WRITE);
 	SPI.transfer (RXB0SIDH);
 	for(byte i=0;i<13;i++) {
-    SPI.transfer(writeVal);
+    	SPI.transfer(writeVal);
   }
 	digitalWrite(CS,HIGH);
 	
@@ -307,7 +308,7 @@ void MCP::clearRxBuffers()
 	SPI.transfer (WRITE);
 	SPI.transfer (RXB1SIDH);
 	for(byte i=0;i<13;i++) {
-    SPI.transfer(writeVal);
+    	SPI.transfer(writeVal);
   }
 	digitalWrite(CS,HIGH);
 }
@@ -322,7 +323,7 @@ void MCP::clearTxBuffers()
 	SPI.transfer (WRITE);
 	SPI.transfer (TXB0SIDH);
 	for(byte i=0;i<13;i++) {
-    SPI.transfer(writeVal);
+    	SPI.transfer(writeVal);
   }
 	digitalWrite(CS,HIGH);
 	
@@ -331,7 +332,7 @@ void MCP::clearTxBuffers()
 	SPI.transfer (WRITE);
 	SPI.transfer (TXB1SIDH);
 	for(byte i=0;i<13;i++) {
-    SPI.transfer(writeVal);
+    	SPI.transfer(writeVal);
   }
 	digitalWrite(CS,HIGH);
 	
@@ -339,7 +340,7 @@ void MCP::clearTxBuffers()
 	SPI.transfer (WRITE);
 	SPI.transfer (TXB2SIDH);
 	for(byte i=0;i<13;i++) {
-    SPI.transfer(writeVal);
+    	SPI.transfer(writeVal);
   }
 	digitalWrite(CS,HIGH);
 }
@@ -420,7 +421,7 @@ Values are as follows
   | 0 | 1 |---|---|---|---|---|---| Message in RXB0
   | 1 | 0 |---|---|---|---|---|---| Message in RXB1
   | 1 | 1 |---|---|---|---|---|---| Message in both buffers* (Buffer 0 has higher priority, 
-									therefore, RXB0 status is reflected in bits 4:0)
+				    therefore, RXB0 status is reflected in bits 4:0)
  
   |bit|bit|bit|bit|bit|bit|bit|bit| Msg Type Received
   | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 | -----------------
