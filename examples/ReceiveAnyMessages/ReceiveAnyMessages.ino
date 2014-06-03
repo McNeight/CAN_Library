330888641dedaf1258c2cb3bc48db46aa3b928e4
Neil McNeight <mcneight+github@gmail.com>
2014-06-02 23:52:01 -0700

    Revised ReceiveAnyMessages.ino and ReceiveCANMessages.ino to use new common API

--- examples/ReceiveAnyMessages/ReceiveAnyMessages.ino
+++ examples/ReceiveAnyMessages/ReceiveAnyMessages.ino
@@ -1,26 +1,23 @@
 /* CAN communications example
- 
+
   Topic: Receive Messages using read(ID, length, data) function
   Author: Pedro Cevallos
   Created: 05/07/14
- 
-  Example shows how to receive messages using MCP2515 CAN controller
+
+  Example shows how to receive messages using CAN
   This example uses Serial Monitor to display received messages
- 
+
   As per wiki information:
-  "CAN bus is a message-based protocol, designed specifically for automotive applications 
+  "CAN bus is a message-based protocol, designed specifically for automotive applications
   but now also used in other areas such as aerospace, maritime, industrial automation and medical equipment."
   For more info http://en.wikipedia.org/wiki/Controller_area_network
- 
-  CAN bit rate:  10 kpbs; 20 kpbs; 50 kbps; 100 kbps; 125 kbps; 250 kbps; 500 kbps; 1000 kbps
-  CAN modes: NORMAL, LISTEN,SLEEP,CONFIG, LOOPBACK
- 
+
  */
 
 #include <Arduino.h>
 #include "CAN.h"
 #if defined(ARDUINO_ARCH_AVR)
-#include <SPI.h> 
+#include <SPI.h>
 #include "CAN_MCP2515.h"
 #elif defined(ARDUINO_ARCH_SAM)
 #include <variant.h>
@@ -29,116 +26,74 @@
 #error “This library only supports boards with an AVR or SAM processor.”
 #endif
 
-// First we define our CAN mode and rate. 
-
-#define mode MCP2515_MODE_NORMAL // define CAN mode
+// First we define our CAN bitrate.
 #define bitrate CAN_BPS_500K // define CAN speed (bitrate)
 
-/* 
+/*
   Second we create CANbus object (CAN channel) and select SPI CS Pin. Do not use "CAN" by itself as it will cause compile errors.
   Can't use CAN0 or CAN1 as variable names, as they are defined in hardware/arduino/sam/system/CMSIS/Device/ATMEL/sam3xa/include/sam3x8e.h
   Needs to be CANbus0, CANbus1, CANbus2, or whatever name you want to give that channel. This can also allow us to create more channels
-  using more MCP2515s as long as we use different SPI CS to control data.
+  using more hardware devices.
  */
 
 #if defined(ARDUINO_ARCH_AVR)
-// Can't use CAN0 or CAN1 as variable names, as they are defined in
 CAN_MCP2515 CANbus(10); // Create CAN channel using pin 10 for SPI chip select
 #elif defined(ARDUINO_ARCH_SAM)
-// Can't use CAN0 or CAN1 as variable names, as they are defined in
 CAN_SAM3X8E CANbus(0);  // Create CAN channel on CAN bus 0
-//CAN1.init(SystemCoreClock, CAN_BPS_500K);
 #else
 #error “This library only supports boards with an AVR or SAM processor.”
 #endif
 
-//CAN_FRAME testFrame;
+void setup()
+{
+  Serial.begin(115200);  // Initialize Serial communications with computer to use serial monitor
 
-void setup() {
-
-  Serial.begin(115200);                                            // Initialize Serial communications with computer to use serial monitor
-
-  //Set CAN mode and speed. Note: Speed is now 500kbit/s so adjust your CAN monitor
+  //Set CAN speed. Note: Speed is now 500kbit/s so adjust your CAN monitor
 
   CANbus.begin(bitrate);
-  
-  delay(4000);                                                    // Delay added just so we can have time to open up Serial Monitor and CAN bus monitor. It can be removed later...
-  
-  if ((CANbus.readMode () == MCP2515_MODE_NORMAL) && (CANbus.readRate() == bitrate))  // Check to see if we set the Mode and speed correctly. For debugging purposes only.
-  {
-    Serial.println("CAN Initialization complete");
-    Serial.print ("CAN speed set to:  ");
-    Serial.print (bitrate);
-    Serial.println ("kbit/s");
-  }
-  else
-  {
-    Serial.println("CAN Initialization failed");
-    Serial.println("CAN speed failed");
-  }
-}
 
+  delay(4000);  // Delay added just so we can have time to open up Serial Monitor and CAN bus monitor. It can be removed later...
+
+  // Output will be formatted as a CSV file, for capture and analysis
+  Serial.println("millis(),ID,Length,Data");
+}
 
 // Create a function to read message and display it through Serial Monitor
 
-void readMessage() {
+void readMessage()
+{
+  unsigned long can_ID; // assign a variable for Message ID
+  byte can_length; //assign a variable for length
+  byte can_data[8] = {0}; //assign an array for data
 
-  unsigned long can_ID;                                       // assign a variable for Message ID
-  byte can_length;                                            //assign a variable for length
-  byte can_data[8];                                           //assign an array for data
-  
-  if (CANbus.available() == true) {                      // Check to see if a valid message has been received.
-    
-    CANbus.read(&can_ID, &can_length, can_data);                        // read Message and assign data through reference operator &
-    
-    if (can_ID == 0x7E8)
+  if (CANbus.available() == true) // Check to see if a valid message has been received.
+  {
+    CANbus.read(&can_ID, &can_length, can_data); // read Message and assign data through reference operator &
+
+    Serial.print(millis());
+    Serial.print(",0x");
+    Serial.print(can_ID, HEX); // Displays received ID
+    Serial.print(",");
+    Serial.print(can_length, HEX); // Displays message length
+    for (byte i = 0; i < can_length; i++)
     {
-      Serial.print("Time | ");
-      Serial.print(millis());
-      Serial.print(" | ID");
-    Serial.print(" | ");
-      Serial.print(can_ID, HEX);                                // Displays received ID
-    Serial.print(" | ");
-    Serial.print("Data Length");
-    Serial.print(" | ");
-      Serial.print(can_length, HEX);                           // Displays message length
-    Serial.print(" | ");
-    Serial.print("Data");
-      for (byte i = 0; i < can_length; i++) {
-      Serial.print(" | ");
-        if (can_data[i] < 0x10)                                 // If the data is less than 10 hex it will assign a zero to the front as leading zeros are ignored...
+      Serial.print(",");
+      if (can_data[i] < 0x10) // If the data is less than 10 hex it will assign a zero to the front as leading zeros are ignored...
       {
-      Serial.print("0");
+        Serial.print("0");
       }
-        Serial.print(can_data[i], HEX);                         // Displays message data
-      
+      Serial.print(can_data[i], HEX); // Displays message data
     }
-    Serial.println();                                     // adds a line
-  }
+    Serial.println(); // adds a line
   }
 }
-    
-void printFrame(CAN_FRAME &frame) {
-   Serial.print("ID: 0x");
-//   Serial.print(frame.id, HEX);
-   Serial.print(" Len: ");
-   Serial.print(frame.length);
-   Serial.print(" Data: 0x");
-   for (int count = 0; count < frame.length; count++) {
-//       Serial.print(frame.data.bytes[count], HEX);
-       Serial.print(" ");
-   }
-   Serial.print("\r\n");
-}
 
-// Finally arduino loop to execute above function with a 100ms delay
 
-void loop() {
+// Finally arduino loop to execute above function with a 50ms delay
 
-  byte J1939_data[] = {0x02, 0x01, 0x0C, 0x00, 0x00, 0x00, 0x00, 0x00};
-  CANbus.write(0x7DF, CAN_BASE_FRAME, 8, J1939_data);
+void loop()
+{
   readMessage();
   delay(50);
-
 }
 
--- examples/ReceiveCANMessages/ReceiveCANMessages.ino
+++ examples/ReceiveCANMessages/ReceiveCANMessages.ino
@@ -1,112 +1,108 @@
 /* CAN communications example
- 
-  Topic: Receive Messages using read(CAN *message) function
+
+  Topic: Receive Messages using read() function
   Author: Pedro Cevallos
   Created: 05/07/14
- 
-  Example shows how to receive messages using MCP2515 CAN controller
+
+  Example shows how to receive messages using CAN
   This example uses Serial Monitor to display received messages
- 
+
   As per wiki information:
-  "CAN bus is a message-based protocol, designed specifically for automotive applications 
+  "CAN bus is a message-based protocol, designed specifically for automotive applications
   but now also used in other areas such as aerospace, maritime, industrial automation and medical equipment."
   For more info http://en.wikipedia.org/wiki/Controller_area_network
- 
-  CAN bit rate:  10 kpbs; 20 kpbs; 50 kbps; 100 kbps; 125 kbps; 250 kbps; 500 kbps; 1000 kbps
-  CAN modes: NORMAL, LISTEN,SLEEP,CONFIG, LOOPBACK
- 
+
  */
 
-#include <can.h> 
-#include <SPI.h> 
+#include <Arduino.h>
+#include "CAN.h"
+#if defined(ARDUINO_ARCH_AVR)
+#include <SPI.h>
+#include "CAN_MCP2515.h"
+#elif defined(ARDUINO_ARCH_SAM)
+#include <variant.h>
+#include "CAN_SAM3X8E.h"
+#else
+#error “This library only supports boards with an AVR or SAM processor.”
+#endif
 
+// First we define our CAN bitrate.
+#define bitrate CAN_BPS_500K // define CAN speed (bitrate)
 
-// First we define our CAN mode and rate. 
-
-#define mode MCP2515_MODE_NORMAL // define CAN mode
-#define bitrate 500 // define CAN speed (bitrate)
-
-/* 
-  Second we create CAN1 object (CAN channel) and select SPI CS Pin. Do not use "CAN" by itself as it will cause compile errors. 
-  Needs to be CAN0, CAN1, CAN2, or whatever name you want to give that channel. This can also allow us to create more channels 
-  using more MCP2515s as long as we use different SPI CS to control data.
+/*
+  Second we create CANbus object (CAN channel) and select SPI CS Pin. Do not use "CAN" by itself as it will cause compile errors.
+  Can't use CAN0 or CAN1 as variable names, as they are defined in hardware/arduino/sam/system/CMSIS/Device/ATMEL/sam3xa/include/sam3x8e.h
+  Needs to be CANbus0, CANbus1, CANbus2, or whatever name you want to give that channel. This can also allow us to create more channels
+  using more hardware devices.
  */
 
-MCP CAN1(10); //Create CAN Channel
-CAN message; // Create message object to use CAN message structure 
+#if defined(ARDUINO_ARCH_AVR)
+CAN_MCP2515 CANbus(10); // Create CAN channel using pin 10 for SPI chip select
+#elif defined(ARDUINO_ARCH_SAM)
+CAN_SAM3X8E CANbus(0);  // Create CAN channel on CAN bus 0
+#else
+#error “This library only supports boards with an AVR or SAM processor.”
+#endif
 
+CAN_FRAME message; // Create message object to use CAN message structure
 
-void setup(){
+void setup()
+{
+  Serial.begin(115200);  // Initialize Serial communications with computer to use serial monitor
 
-  Serial.begin(115200);                                          // Initialize Serial communications with computer to use serial monitor
+  //Set CAN speed. Note: Speed is now 500kbit/s so adjust your CAN monitor
 
-  //Set CAN mode and speed. Note: Speed is now 500kbit/s so adjust your CAN monitor
+  CANbus.begin(bitrate);
 
-  CAN1.begin(mode, bitrate);
-  
-  delay(4000);                                                   // Delay added just so we can have time to open up Serial Monitor and CAN bus monitor. It can be removed later...
-  
- if ((CAN1.readMode () == mode) && (CAN1.readRate() == bitrate)) // Check to see if we set the Mode and speed correctly. For debugging purposes only.
-  {
-    Serial.println("CAN Initialization complete");
-    Serial.print ("CAN speed set to:  ");
-    Serial.print (bitrate);
-    Serial.println ("kbit/s");
-  }
-  else
-  {
-    Serial.println("CAN Initialization failed");
-    Serial.println("CAN speed failed");
-  }
+  delay(4000);  // Delay added just so we can have time to open up Serial Monitor and CAN bus monitor. It can be removed later...
+
+  // Output will be formatted as a CSV file, for capture and analysis
+  Serial.println("millis(),ID,RTR,EID,Length,Data");
 }
-
 
 // Create a function to read message and display it through Serial Monitor
 
-void readMessage(){
- 
-if (CAN1.msgAvailable() == true){                               // Check to see if a valid message has been received.
+void readMessage()
+{
+  if (CANbus.available() == true) // Check to see if a valid message has been received.
+  {
+    message = CANbus.read(); //read message, it will follow the CAN structure of ID,RTR, legnth, data. Allows both Extended or Standard
 
-    CAN1.read(&message);                                        //read message, it will follow the CAN structure of ID,RTR, legnth, data. Allows both Extended or Standard
-
-    Serial.print("ID");
-    Serial.print(" | ");
-    Serial.print(message.ID,HEX);                               //display message ID
-    Serial.print(" | ");
-    Serial.print("RTR");
-    Serial.print(" | ");
-    Serial.print(message.rtr,HEX);                              //display message RTR
-   if (message.rtr == 1){
-     Serial.print(" REMOTE REQUEST MESSAGE ");                  //technically if its RTR frame/message it will not have data So display this
-   }else {
-    Serial.print(" | ");
-    Serial.print("Length");
-    Serial.print(" | ");
-    Serial.print(message.length,HEX);                            //display message length
-    Serial.print(" | ");
-    Serial.print("Data");
-    for (byte i=0;i<message.length;i++) {
-      Serial.print(" | ");
-      if(message.data[i] <0x10)
-      {
-        Serial.print("0");
-      }
-      Serial.print(message.data[i],HEX);                          //display data based on length
+    Serial.print(millis());
+    Serial.print(",0x");
+    Serial.print(message.id, HEX); //display message ID
+    Serial.print(",");
+    Serial.print(message.rtr); //display message RTR
+    Serial.print(",");
+    Serial.print(message.extended); //display message EID
+    Serial.print(",");
+    if (message.rtr == 1)
+    {
+      Serial.print(" REMOTE REQUEST MESSAGE "); //technically if its RTR frame/message it will not have data So display this
     }
-   }
-    Serial.println();
-}   
+    else
+    {
+      Serial.print(message.length, HEX); //display message length
+      Serial.print(",");
+      for (byte i = 0; i < message.length; i++)
+      {
+        Serial.print(",");
+        if (message.data[i] < 0x10) // If the data is less than 10 hex it will assign a zero to the front as leading zeros are ignored...
+        {
+          Serial.print("0");
+        }
+        Serial.print(message.data[i], HEX); //display data based on length
+      }
+    }
+    Serial.println(); // adds a line
+  }
 }
-    
 
-// Finally arduino loop to execute above function with a 150ms delay
+// Finally arduino loop to execute above function with a 50ms delay
 
-void loop(){
-
-  byte J1939_data[] = {0x02, 0x01, 0x0C, 0x55};
-  CAN1.send(0x7DF, extID, 4, J1939_data);
+void loop()
+{
   readMessage();
-  delay(150);
-
+  delay(50);
 }
 
